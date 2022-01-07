d95b6212a1474f7c7c7dde483f1880e57b91d46a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-01-07 18:45:29 +0800

    init

---
+++ src/test/java/cn/com/xuxiaowei/SpringBootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
